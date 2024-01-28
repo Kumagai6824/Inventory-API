04d0bc73a75eb9243dd6ede7eba601587d6b8d69
Kumagai-java2211 <j.min.howsand682@gmail.com>
2024-01-28 20:47:07 +0900

    BadRequestについてExceptionHandlerクラスへハンドラー追加

--- src/main/java/com/raisetech/inventoryapi/ProductExceptionHandler.java
+++ src/main/java/com/raisetech/inventoryapi/ProductExceptionHandler.java
@@ -24,4 +24,17 @@ public class ProductExceptionHandler {
                 "path", request.getRequestURI());
         return new ResponseEntity(body, HttpStatus.NOT_FOUND);
     }
+
+    @ExceptionHandler(value = BadRequestException.class)
+    public ResponseEntity<Map<String, String>> handleBadRequest(
+            BadRequestException e, HttpServletRequest request) {
+
+        Map<String, String> body = Map.of(
+                "timestamp", ZonedDateTime.now().toString(),
+                "status", String.valueOf(HttpStatus.BAD_REQUEST.value()),
+                "error", HttpStatus.BAD_REQUEST.getReasonPhrase(),
+                "message", e.getMessage(),
+                "path", request.getRequestURI());
+        return new ResponseEntity(body, HttpStatus.BAD_REQUEST);
+    }
 }
