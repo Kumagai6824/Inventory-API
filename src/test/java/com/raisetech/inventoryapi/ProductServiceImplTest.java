77bcedc916a6ca0fc9dd5d1db1202b34704f45b7
Kumagai-java2211 <j.min.howsand682@gmail.com>
2024-02-17 14:08:33 +0900

    引数修正Integer -> int・@ValueSource修正

--- src/test/java/com/raisetech/inventoryapi/ProductServiceImplTest.java
+++ src/test/java/com/raisetech/inventoryapi/ProductServiceImplTest.java
@@ -61,7 +61,7 @@ class ProductServiceImplTest {
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"1", "100"})
+    @ValueSource(ints = {1, 100})
     public void 商品IDを指定して更新したときに商品情報が更新されること(int id) throws Exception {
         String initialName = "Bolt";
         String renewedName = "Shaft";
@@ -72,7 +72,7 @@ class ProductServiceImplTest {
 
     @ParameterizedTest
     @ValueSource(ints = {0})
-    public void 存在しない商品IDを指定して更新したときに期待通り例外を返すこと(Integer id) throws Exception {
+    public void 存在しない商品IDを指定して更新したときに期待通り例外を返すこと(int id) throws Exception {
         String renewedName = "Shaft";
         when(productMapper.findById(id)).thenReturn(Optional.empty());
         assertThatThrownBy(() -> productServiceImpl.updateProductById(id, renewedName))
