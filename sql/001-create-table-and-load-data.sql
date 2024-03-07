182f1420a986e4e07b281a0f02b18ef1b05987ba
Kumagai-java2211 <j.min.howsand682@gmail.com>
2024-03-07 23:05:04 +0900

    inventoryProductsテーブルの追加

--- sql/001-create-table-and-load-data.sql
+++ sql/001-create-table-and-load-data.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS inventoryProducts;
 DROP TABLE IF EXISTS products;
 
 CREATE TABLE products (
@@ -8,3 +9,16 @@ CREATE TABLE products (
 
 INSERT INTO products (id, name) VALUES (1, "Bolt 1");
 INSERT INTO products (id, name) VALUES (2, "Washer");
+
+CREATE TABLE inventoryProducts (
+  id int unsigned AUTO_INCREMENT,
+  product_id int,
+  quantity int DEFAULT 0,
+  history DATETIME NOT NULL,
+  PRIMARY KEY(id),
+  CONSTRAINT fk_product_id FOREIGN KEY (product_id) REFERENCES products (id)
+);
+
+INSERT INTO inventoryProducts (product_id, quantity, history) VALUES (1, 100, '2023-12-10 23:58:10');
+INSERT INTO inventoryProducts (product_id, quantity, history) VALUES (2, 200, '2024-1-10 12:58:10');
+
