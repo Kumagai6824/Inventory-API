64623fcc3eb3f9487b3d9fa2e578eb5b032f209d
Kumagai-java2211 <j.min.howsand682@gmail.com>
2024-03-25 23:06:50 +0900

    InventoryProductServiceImplの実装/getQuantityByProductIdの実装

---
+++ src/main/java/com/raisetech/inventoryapi/service/InventoryProductServiceImpl.java
@@ -0,0 +1,20 @@
+package com.raisetech.inventoryapi.service;
+
+import com.raisetech.inventoryapi.exception.ResourceNotFoundException;
+import com.raisetech.inventoryapi.mapper.InventoryProductMapper;
+import org.springframework.stereotype.Service;
+
+@Service
+public class InventoryProductServiceImpl implements InventoryProductService {
+    private final InventoryProductMapper inventoryProductMapper;
+
+    public InventoryProductServiceImpl(InventoryProductMapper inventoryProductMapper) {
+        this.inventoryProductMapper = inventoryProductMapper;
+    }
+
+    @Override
+    public Integer getQuantityByProductId(int productId) {
+        return this.inventoryProductMapper.getQuantityByProductId(productId).orElseThrow(() -> new ResourceNotFoundException("Product ID:" + productId + " does not exist"));
+    }
+
+}
