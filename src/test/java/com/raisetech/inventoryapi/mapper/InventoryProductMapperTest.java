e4b38575d9c5d143c7bd1b2e8c38a7c18cc715fe
Kumagai-java2211 <j.min.howsand682@gmail.com>
2024-04-16 08:18:41 +0900

    InventoryProductMapperTestにて@Sql（DBセットアップ）をクラスへ付与へ変更

--- src/test/java/com/raisetech/inventoryapi/mapper/InventoryProductMapperTest.java
+++ src/test/java/com/raisetech/inventoryapi/mapper/InventoryProductMapperTest.java
@@ -11,15 +11,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @MybatisTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Sql(
+        scripts = {"classpath:/delete-products.sql", "classpath:/insert-products.sql", "classpath:/delete-inventory-products.sql", "classpath:/insert-inventory-products.sql"},
+        executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD
+)
 class InventoryProductMapperTest {
     @Autowired
     InventoryProductMapper inventoryProductMapper;
 
     @Test
-    @Sql(
-            scripts = {"classpath:/delete-products.sql", "classpath:/insert-products.sql", "classpath:/delete-inventory-products.sql", "classpath:/insert-inventory-products.sql"},
-            executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD
-    )
     @Transactional
     void 合計在庫数が取得できること() {
         int id = 1;
