64f6c4e6dccfe90504b9ad330eb35936a78b6a76
Kumagai-java2211 <j.min.howsand682@gmail.com>
2024-04-15 20:36:26 +0900

    InventoryProductMapperテスト実装

---
+++ src/test/java/com/raisetech/inventoryapi/mapper/InventoryProductMapperTest.java
@@ -0,0 +1,30 @@
+package com.raisetech.inventoryapi.mapper;
+
+import org.junit.jupiter.api.Test;
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@MybatisTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class InventoryProductMapperTest {
+    @Autowired
+    InventoryProductMapper inventoryProductMapper;
+
+    @Test
+    @Sql(
+            scripts = {"classpath:/delete-products.sql", "classpath:/insert-products.sql", "classpath:/delete-inventory-products.sql", "classpath:/insert-inventory-products.sql"},
+            executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD
+    )
+    @Transactional
+    void 合計在庫数が取得できること() {
+        int id = 1;
+        Integer quantity = inventoryProductMapper.getQuantityByProductId(id);
+        assertThat(quantity).isEqualTo(100);
+    }
+
+}
