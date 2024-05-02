7534af5a758d8069de9a0ad8c21f5c7a7fcbd7f3
Kumagai-java2211 <j.min.howsand682@gmail.com>
2024-05-02 09:36:28 +0900

    MapperTestにて物理削除のテスト削除

--- src/test/java/com/raisetech/inventoryapi/mapper/ProductMapperTest.java
+++ src/test/java/com/raisetech/inventoryapi/mapper/ProductMapperTest.java
@@ -98,38 +98,22 @@ class ProductMapperTest {
         assertThat(productMapper.findById(1)).contains(new Product(1, "Shaft", null));
     }
 
-    @Test
+/*    @Test
     @Sql(
             scripts = {"classpath:/delete-products.sql", "classpath:/insert-products.sql"},
             executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD
     )
     @Transactional
-    void 削除処理が完了して商品情報が削除されること() {
-        productMapper.deleteProductById(1);
+    void 論理削除でdeletedAtに処理日時が入ること() {
+        productMapper.logicalDeleteProductById(1);
         List<Product> products = productMapper.findAll();
-        assertThat(products)
-                .hasSize(1)
-                .contains(
-                        new Product(2, "Washer", null)
-                );
-    }
 
-    @Test
-    @Sql(
-            scripts = {"classpath:/delete-products.sql", "classpath:/insert-products.sql"},
-            executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD
-    )
-    @Transactional
-    void 削除処理時に存在しないIDを指定した場合何も削除されないこと() {
-        productMapper.deleteProductById(3);
-        List<Product> products = productMapper.findAll();
-        assertThat(productMapper.findById(3)).isEmpty();
         assertThat(products)
                 .hasSize(2)
                 .contains(
-                        new Product(1, "Bolt 1", null),
+                        new Product(1, "Bolt 1", ),
                         new Product(2, "Washer", null)
                 );
-    }
+    }*/
 
 }
