30289aed8b3fc85448143ba56a85a34f2a23a699
Kumagai-java2211 <j.min.howsand682@gmail.com>
2024-05-13 21:10:07 +0900

    在庫履歴取得/ServiceとServiceImplの実装

--- src/main/java/com/raisetech/inventoryapi/service/ProductServiceImpl.java
+++ src/main/java/com/raisetech/inventoryapi/service/ProductServiceImpl.java
@@ -1,5 +1,6 @@
 package com.raisetech.inventoryapi.service;
 
+import com.raisetech.inventoryapi.entity.InventoryHistory;
 import com.raisetech.inventoryapi.entity.Product;
 import com.raisetech.inventoryapi.exception.InventoryStillExistsException;
 import com.raisetech.inventoryapi.exception.ResourceNotFoundException;
@@ -50,5 +51,10 @@ public class ProductServiceImpl implements ProductService {
         } else {
             throw new InventoryStillExistsException("Cannot delete Product because the quantity is not 0");
         }
+    }
+
+    @Override
+    public List<InventoryHistory> findHistoriesByProductId(int id) {
+        return productMapper.findHistoriesByProductId(id);
     }
 }
