7001e28229acbd7cea203ee219ee0a27a1091351
Kumagai-java2211 <j.min.howsand682@gmail.com>
2024-05-29 22:36:00 +0900

    Error:InventoryProductMapperTestにて在庫登録処理テスト追加

--- src/test/java/com/raisetech/inventoryapi/mapper/InventoryProductMapperTest.java
+++ src/test/java/com/raisetech/inventoryapi/mapper/InventoryProductMapperTest.java
@@ -1,5 +1,6 @@
 package com.raisetech.inventoryapi.mapper;
 
+import com.raisetech.inventoryapi.entity.InventoryProduct;
 import org.junit.jupiter.api.Test;
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +8,10 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.OffsetDateTime;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @MybatisTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
@@ -33,6 +37,21 @@ class InventoryProductMapperTest {
         int id = 0;
         Integer quantity = inventoryProductMapper.getQuantityByProductId(id);
         assertThat(quantity).isEqualTo(0);
+    }
+
+    @Test
+    @Transactional
+    void 登録した在庫情報と新しく採番されたIDが設定されること() {
+        int productId = 1;
+        InventoryProduct inventoryProduct = new InventoryProduct();
+        inventoryProduct.setProductId(productId);
+        inventoryProduct.setQuantity(500);
+        inventoryProductMapper.createInventoryProduct(inventoryProduct);
+
+        int id = inventoryProduct.getId();
+        OffsetDateTime expectedDateTime = inventoryProduct.getHistory();
+        assertNotNull(id);
+        assertThat(inventoryProductMapper.findInventoryByProductId(productId)).contains(new InventoryProduct(id, productId, 500, expectedDateTime));
     }
 
 }
