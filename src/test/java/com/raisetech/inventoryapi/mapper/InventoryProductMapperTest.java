510fc22964f0d48070c1eb6737bd04eab7ed242b
Kumagai-java2211 <j.min.howsand682@gmail.com>
2024-06-08 22:48:22 +0900

    IP.MapperTestにて@ExpectedDataSetの記載を戻した

--- src/test/java/com/raisetech/inventoryapi/mapper/InventoryProductMapperTest.java
+++ src/test/java/com/raisetech/inventoryapi/mapper/InventoryProductMapperTest.java
@@ -46,6 +46,7 @@ class InventoryProductMapperTest {
 
     @Test
     @Transactional
+    @ExpectedDataSet(value = "/dataset/expectedInventoryProductId1.yml", ignoreCols = {"history"})
     void 商品IDと一致する在庫情報を取得できること() {
         int productId = 1;
         InventoryProduct inventoryProduct = new InventoryProduct();
