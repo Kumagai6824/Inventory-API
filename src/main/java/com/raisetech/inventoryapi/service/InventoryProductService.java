28032a07e3fbf39b114d2f85e895a968f7b21996
Kumagai-java2211 <j.min.howsand682@gmail.com>
2024-06-25 08:25:09 +0900

    IP.ServiceとIP.ServiceImplにて在庫登録（入庫）処理実装

--- src/main/java/com/raisetech/inventoryapi/service/InventoryProductService.java
+++ src/main/java/com/raisetech/inventoryapi/service/InventoryProductService.java
@@ -1,5 +1,9 @@
 package com.raisetech.inventoryapi.service;
 
+import com.raisetech.inventoryapi.entity.InventoryProduct;
+
 public interface InventoryProductService {
     Integer getQuantityByProductId(int productId);
+
+    void receivingInventoryProduct(InventoryProduct inventoryProduct);
 }
--- src/main/java/com/raisetech/inventoryapi/service/InventoryProductServiceImpl.java
+++ src/main/java/com/raisetech/inventoryapi/service/InventoryProductServiceImpl.java
@@ -1,20 +1,41 @@
 package com.raisetech.inventoryapi.service;
 
+import com.raisetech.inventoryapi.entity.InventoryProduct;
+import com.raisetech.inventoryapi.entity.Product;
+import com.raisetech.inventoryapi.exception.ResourceNotFoundException;
 import com.raisetech.inventoryapi.mapper.InventoryProductMapper;
+import com.raisetech.inventoryapi.mapper.ProductMapper;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class InventoryProductServiceImpl implements InventoryProductService {
     private final InventoryProductMapper inventoryProductMapper;
+    private final ProductMapper productMapper;
 
-    public InventoryProductServiceImpl(InventoryProductMapper inventoryProductMapper) {
+    public InventoryProductServiceImpl(InventoryProductMapper inventoryProductMapper, ProductMapper productMapper) {
         this.inventoryProductMapper = inventoryProductMapper;
+        this.productMapper = productMapper;
     }
-    
+
     @Override
     public Integer getQuantityByProductId(int productId) {
         Integer quantity = this.inventoryProductMapper.getQuantityByProductId(productId);
         return quantity;
+    }
+
+    @Override
+    public void receivingInventoryProduct(InventoryProduct inventoryProduct) {
+        int productId = inventoryProduct.getProductId();
+        Optional<Product> productOptional = productMapper.findById(productId);
+
+        Product product = productOptional.orElseThrow(() -> new ResourceNotFoundException("Product ID:" + productId + " does not exist"));
+        if (product.getDeletedAt() != null) {
+            throw new ResourceNotFoundException("Product ID:" + productId + " does not exist");
+        }
+
+        inventoryProductMapper.createInventoryProduct(inventoryProduct);
     }
 
 }
